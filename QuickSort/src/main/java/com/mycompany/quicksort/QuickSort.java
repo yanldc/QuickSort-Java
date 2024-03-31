621aa7898224e32e658d613bb8ef458f75463d8e
Yan Lemke de Castro <112890992+yanldc@users.noreply.github.com>
2024-03-31 14:12:11 -0300

    Add files via upload

---
+++ QuickSort/src/main/java/com/mycompany/quicksort/QuickSort.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.quicksort;
+
+/**
+ *
+ * @author yan-l
+ */
+public class QuickSort {
+
+    public void sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private void quickSort(int[] array, int menor, int maior) {
+        if (menor < maior) {
+            int partitionIndex = particao(array, menor, maior);
+            quickSort(array, menor, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, maior);
+        }
+    }
+
+    private int particao(int[] array, int menor, int maior) {
+        int pivo = array[maior];
+        int i = menor - 1;
+        for (int j = menor; j < maior; j++) {
+            if (array[j] < pivo) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[maior];
+        array[maior] = temp;
+        return i + 1;
+    }
+
+    public void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
+
